09d666ed43134914c5302177bc62d82955352569
sanoopv <sanoopv@gmail.com>
2017-10-06 14:53:48 +0530

    refactor

--- jquery.highlight.js
+++ jquery.highlight.js
@@ -58,8 +58,8 @@ jQuery.extend({
                 return 1; //skip added node in parent
             }
         } else if ((node.nodeType === 1 && node.childNodes) && // only element nodes that have children
-                !/(script|style)/i.test(node.tagName) && // ignore script and style nodes
-                !(node.tagName === nodeName.toUpperCase() && node.className === className)) { // skip if already highlighted
+            !/(script|style)/i.test(node.tagName) && // ignore script and style nodes
+            !(node.tagName === nodeName.toUpperCase() && node.className === className)) { // skip if already highlighted
             for (var i = 0; i < node.childNodes.length; i++) {
                 i += jQuery.highlight(node.childNodes[i], re, nodeName, className);
             }
@@ -69,10 +69,13 @@ jQuery.extend({
 });
 
 jQuery.fn.unhighlight = function (options) {
-    var settings = { className: 'highlight', element: 'span' };
+    var settings = {
+        className: 'highlight',
+        element: 'span'
+    };
     jQuery.extend(settings, options);
 
-    return this.find(settings.element + "." + settings.className).each(function () {
+    return this.find(settings.element + '.' + settings.className).each(function () {
         var parent = this.parentNode;
         parent.replaceChild(this.firstChild, this);
         parent.normalize();
@@ -80,27 +83,33 @@ jQuery.fn.unhighlight = function (options) {
 };
 
 jQuery.fn.highlight = function (words, options) {
-    var settings = { className: 'highlight', element: 'span', caseSensitive: false, wordsOnly: false };
+    var settings = {
+        className: 'highlight',
+        element: 'span',
+        caseSensitive: false,
+        wordsOnly: false
+    };
     jQuery.extend(settings, options);
-    
+
     if (words.constructor === String) {
         words = [words];
     }
-    words = jQuery.grep(words, function(word, i){
-      return word != '';
+    words = jQuery.grep(words, function (word) {
+        return word != '';
     });
-    words = jQuery.map(words, function(word, i) {
-      return word.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+    words = jQuery.map(words, function (word) {
+        return word.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
     });
-    if (words.length == 0) { return this; };
-
-    var flag = settings.caseSensitive ? "" : "i";
-    var pattern = "(" + words.join("|") + ")";
+    if (words.length == 0) {
+        return this;
+    }
+    var flag = settings.caseSensitive ? '' : 'i';
+    var pattern = '(' + words.join('|') + ')';
     if (settings.wordsOnly) {
-        pattern = "\\b" + pattern + "\\b";
+        pattern = '\\b' + pattern + '\\b';
     }
     var re = new RegExp(pattern, flag);
-    
+
     return this.each(function () {
         jQuery.highlight(this, re, settings.element, settings.className);
     });
